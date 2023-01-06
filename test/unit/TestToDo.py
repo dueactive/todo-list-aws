939c8ad0134b3cb7600f043401b4b295e24abe52
dueactive <37436467+dueactive@users.noreply.github.com>
2023-01-06 20:32:44 +0100

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -104,6 +104,17 @@ class TestDatabaseFunctions(unittest.TestCase):
             responseGet['text'])
         print ('End: test_get_todo')
         
+     
+     def test_get_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_get_todo_error')
+        # Testing file functions
+        from src.todoList import get_item
+        # Table mock
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        print ('End: test_get_todo_error')
+        
         
     
     def test_list_todo(self):
@@ -121,6 +132,15 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertTrue(result[0]['text'] == self.text)
         print ('End: test_list_todo')
         
+        
+      def test_list_todo_error(self):
+        print('---------------------')
+        print('Start: test_list_todo_error')
+        # Testing file functions
+        from src.todoList import get_items
+        # Table mock
+        self.assertRaises(Exception, get_items("", self.dynamodb))
+        print('End: test_list_todo_error')  
 
 
     def test_update_todo(self):
